2ca3684554cfeec6a684658c37cbbd1a361f8fee
MiYan <quyunshuo@qq.com>
2019-08-15 10:53:42 +0800

    AnimationUseActivity 完成

--- brvahrecyclerview/src/main/java/com/quyunshuo/brvahrecyclerview/AnimationUseActivity.java
+++ brvahrecyclerview/src/main/java/com/quyunshuo/brvahrecyclerview/AnimationUseActivity.java
@@ -1,14 +1,135 @@
 package com.quyunshuo.brvahrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.media.session.PlaybackState;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.jaredrummler.materialspinner.MaterialSpinner;
+import com.kyleduo.switchbutton.SwitchButton;
+import com.quyunshuo.brvahrecyclerview.adapter.AnimationAdapter;
+import com.quyunshuo.brvahrecyclerview.animation.CustomAnimation;
+import com.quyunshuo.brvahrecyclerview.data.Status;
 
 public class AnimationUseActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private AnimationAdapter mAdapter;
+    private ImageView mImageBack;
+    private MaterialSpinner mSpinner;
+    private SwitchButton mSwitchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_use);
+        initView();
+        initData();
+        initMenu();
+
+    }
+
+    private void initView() {
+        mImageBack = findViewById(R.id.img_back);
+        mSwitchButton = findViewById(R.id.switch_button);
+        mRecyclerView = findViewById(R.id.recycler);
+        mSpinner = findViewById(R.id.spinner);
+        //设置LayoutManager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setHasFixedSize(true);
+        mImageBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    //初始化Menu
+    private void initMenu() {
+        //设置Menu的item
+        mSpinner.setItems("AlphaIn", "ScaleIn", "SlideInBottom", "SlideInLeft", "SlideInRight", "Custom");
+        mSpinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(MaterialSpinner view, int position, long id, Object item) {
+                switch (position) {
+                    case 0:
+                        mAdapter.openLoadAnimation(BaseQuickAdapter.ALPHAIN);
+                        break;
+                    case 1:
+                        mAdapter.openLoadAnimation(BaseQuickAdapter.SCALEIN);
+                        break;
+                    case 2:
+                        mAdapter.openLoadAnimation(BaseQuickAdapter.SLIDEIN_BOTTOM);
+                        break;
+                    case 3:
+                        mAdapter.openLoadAnimation(BaseQuickAdapter.SLIDEIN_LEFT);
+                        break;
+                    case 4:
+                        mAdapter.openLoadAnimation(BaseQuickAdapter.SLIDEIN_RIGHT);
+                        break;
+                    case 5:
+                        mAdapter.openLoadAnimation(new CustomAnimation());
+                        break;
+                    default:
+                        break;
+                }
+                //设置完加载动画时重新设置适配器
+                mRecyclerView.setAdapter(mAdapter);
+            }
+        });
+        //init FirstOnly
+        mAdapter.isFirstOnly(false);
+        //
+        mSwitchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    mAdapter.isFirstOnly(true);
+                } else {
+                    mAdapter.isFirstOnly(false);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void initData() {
+        mAdapter = new AnimationAdapter();
+        //打开加载动画
+        mAdapter.openLoadAnimation();
+        //设置不做animation的个数
+        mAdapter.setNotDoAnimationCount(3);
+        //设置点击事件
+        mAdapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
+            @Override
+            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+                String content = null;
+                Status status = (Status) adapter.getItem(position);
+                switch (view.getId()) {
+                    case R.id.imgView:
+                        content = "imgView:" + status.getUserAvatar();
+                        Toast.makeText(AnimationUseActivity.this, content, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.text:
+                        content = "name:" + status.getUserName();
+                        Toast.makeText(AnimationUseActivity.this, content, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.text2:
+                        content = "tweetText:" + status.getUserName();
+                        Toast.makeText(AnimationUseActivity.this, content, Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
--- brvahrecyclerview/src/main/java/com/quyunshuo/brvahrecyclerview/adapter/AnimationAdapter.java
+++ brvahrecyclerview/src/main/java/com/quyunshuo/brvahrecyclerview/adapter/AnimationAdapter.java
@@ -4,6 +4,7 @@ import android.text.TextPaint;
 import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -17,22 +18,22 @@ import com.quyunshuo.brvahrecyclerview.utils.SpannableStringUtils;
 import com.quyunshuo.brvahrecyclerview.utils.ToastUtils;
 import com.quyunshuo.brvahrecyclerview.utils.Utils;
 
-import java.util.List;
-
 /**
  * @Author: MiYan
  * @Time: 2019/8/14 20:37
  * @Description: 描述：
  */
 public class AnimationAdapter extends BaseQuickAdapter<Status, BaseViewHolder> {
-
-    public AnimationAdapter(int layoutResId, @Nullable List<Status> data) {
+    //构造方法里直接传入了item布局和数据源
+    public AnimationAdapter() {
         super(R.layout.animation_item, DataServer.getSampleData(100));
     }
 
     @Override
     protected void convert(BaseViewHolder helper, Status item) {
+        //给指定控件set点击事件
         helper.addOnClickListener(R.id.imgView).addOnClickListener(R.id.text);
+        //item背景使用switch()设置
         switch (helper.getLayoutPosition() % 3) {
             case 0:
                 helper.setImageResource(R.id.imgView, R.mipmap.animation_img1);
@@ -46,24 +47,30 @@ public class AnimationAdapter extends BaseQuickAdapter<Status, BaseViewHolder> {
             default:
                 break;
         }
+        //给item的标题设置text
         helper.setText(R.id.text, "Hoteis in Rio de Janeiro");
         String msg = "\"He was one of Australia's most of distinguished artistes, renowned for his portraits\"";
-        ((TextView) helper.getView(R.id.text)).setText(SpannableStringUtils.getBuilder(msg).append("landscapes and nedes").setClickSpan(clickableSpan).create());
-        ((TextView) helper.getView(R.id.text)).setMovementMethod(ClickableMovementMethod.getInstance());
-        ((TextView) helper.getView(R.id.text)).setFocusable(false);
-        ((TextView) helper.getView(R.id.text)).setClickable(false);
-        ((TextView) helper.getView(R.id.text)).setLongClickable(false);
+
+        ((TextView) helper.getView(R.id.text2)).setText(SpannableStringUtils.getBuilder(msg).append("landscapes and nedes").setClickSpan(clickableSpan).create());
+        ((TextView) helper.getView(R.id.text2)).setMovementMethod(ClickableMovementMethod.getInstance());
+        ((TextView) helper.getView(R.id.text2)).setFocusable(false);
+        //设置不可点击
+        ((TextView) helper.getView(R.id.text2)).setClickable(false);
+        //设置不可长按
+        ((TextView) helper.getView(R.id.text2)).setLongClickable(false);
     }
 
     private ClickableSpan clickableSpan = new ClickableSpan() {
+        //点击事件
         @Override
         public void onClick(View widget) {
-            ToastUtils.showShortToast("事件触发了 landscapes and nedes");
+//            ToastUtils.showShortToast("事件触发了 landscapes and nedes");//使程序崩溃
+            Toast.makeText(mContext, "事件触发了 landscapes and nedes", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void updateDrawState(TextPaint ds) {
-            ds.setColor(Utils.getContext().getResources().getColor(R.color.clickspan_color));
+            ds.setColor(mContext.getResources().getColor(R.color.clickspan_color));
             ds.setUnderlineText(true);
         }
     };
