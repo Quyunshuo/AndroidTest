107510871a7b08b735782cd657775d8ed834b86c
Quyunshuo <quyunshuo@qq.com>
2019-10-14 12:32:34 +0800

    升级Gradle

--- objectanimator/src/main/java/com/quyunshuo/objectanimator/MainActivity.java
+++ objectanimator/src/main/java/com/quyunshuo/objectanimator/MainActivity.java
@@ -1,11 +1,11 @@
 package com.quyunshuo.objectanimator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
