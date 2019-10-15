712b65d9cf5f7579134aa790bda259a0353b32ac
Quyunshuo <quyunshuo@qq.com>
2019-10-15 20:39:00 +0800

    二阶贝塞尔曲线实现波纹

--- customview/src/main/java/com/quyunshuo/customview/MainActivity.java
+++ customview/src/main/java/com/quyunshuo/customview/MainActivity.java
@@ -13,6 +13,7 @@ import com.quyunshuo.customview.view.MyViewFour;
 import com.quyunshuo.customview.view.MyViewSix;
 import com.quyunshuo.customview.view.MyViewThree;
 import com.quyunshuo.customview.view.MyViewTwo;
+import com.quyunshuo.customview.view.RippleView;
 
 public class MainActivity extends AppCompatActivity {
     private FrameLayout root;
@@ -27,14 +28,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        root = findViewById(R.id.frame_layout);
-        myView = findViewById(R.id.my_view);
-        findViewById(R.id.reset).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myView.reset();
-            }
-        });
+//        root = findViewById(R.id.frame_layout);
+//        myView = findViewById(R.id.my_view);
+//        findViewById(R.id.reset).setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                myView.reset();
+//            }
+//        });
+        final RippleView myView = findViewById(R.id.right_view);
+        myView.startAnim();
     }
 
     private void initDraw() {
---
+++ customview/src/main/java/com/quyunshuo/customview/view/RippleView.java
@@ -0,0 +1,67 @@
+package com.quyunshuo.customview.view;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+/**
+ * @Author: MiYan
+ * @Time: 2019/10/15 20:28
+ * @Description: 描述：
+ */
+public class RippleView extends View {
+    private Paint mPaint;
+    private Path mPath;
+    private int mItemWaveLength = 1000;
+    private int dx;
+
+    public RippleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPath = new Path();
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPath.reset();
+        int originY = 1200;
+        int halfWaveLen = mItemWaveLength / 2;
+        mPath.moveTo(-mItemWaveLength + dx, originY);
+        for (int i = -mItemWaveLength; i <= getWidth() + mItemWaveLength; i += mItemWaveLength) {
+            mPath.rQuadTo(halfWaveLen / 2, -100, halfWaveLen, 0);
+            mPath.rQuadTo(halfWaveLen / 2, 100, halfWaveLen, 0);
+        }
+        mPath.lineTo(getWidth(), getHeight());
+        mPath.lineTo(0, getHeight());
+        mPath.close();
+
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    /**
+     * 开启动画
+     */
+    public void startAnim() {
+        ValueAnimator animator = ValueAnimator.ofInt(0, mItemWaveLength);
+        animator.setDuration(2000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                dx = (int) animation.getAnimatedValue();
+                postInvalidate();
+            }
+        });
+        animator.start();
+    }
+}
